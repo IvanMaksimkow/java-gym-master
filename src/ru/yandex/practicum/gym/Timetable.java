ab3fb798fd62f836dd717b6fb342b01b936fc275
IvanMaksimkow <ivanmaksimkow1973@gmail.com>
2026-06-11 13:38:44 +0300

    Update Timetable.java

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -3,6 +3,9 @@ package ru.yandex.practicum.gym;
 import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Comparator;
 
 public class Timetable {
 
